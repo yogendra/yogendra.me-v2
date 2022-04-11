056ee7648f0248a1d55081979fe5edf003e6c609
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-04-11 05:26:47 +0000

    Adding killall command

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$( cd ${SCRIPT_DIR}/.. ; pwd)
 
 direnv allow
+killall hugo
 echo "Launching Hugo..."
 nohup hugo  --gc --enableGitInfo -DFE  --cleanDestinationDir -e local serve &> hugo.log &
